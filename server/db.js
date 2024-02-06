68c99c9b8f31635de1dd72462874ceba9c136ce7
kesle yrn <kesachyrn@gmail.com>
2024-02-06 21:28:12 +0200

    commit for db

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+user: "postgress",
+password:"kg",
+host:"localhost",
+port:5432,
+database:perntodolist,
+})
+
+module.exports = pool; 
--- server/index.js
+++ server/index.js
@@ -1,10 +1,34 @@
         const express = require("express");
         const app = express();
         const cors = require("cors");
+        const pool = ("./db");
 
         // middleware
         app.use(cors());
         app.use(express.json());
+
+        //Routes
+
+        //create todo
+
+        app.post("./todo",async (req,res) =>{
+            try {
+               const { description } = req.body;
+               const newTodo =await pool.query(
+                "INSERT INTO todo (description) VALUES ($1) RETURNING *",
+                 [ description ]
+               );
+              res.json(newTodo.rows[0]);
+            } catch (error) {
+                console.error(err.message);
+            }
+        })
+
+        //get all  todos
+
+        //update a todo
+
+        //delete a todo
 
         app.listen(5000 ,() =>{
             console.log ("server has started on port 5000")
