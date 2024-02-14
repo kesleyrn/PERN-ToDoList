7e5c0db81d5d89a8e31eabc2b824b6a7691b24c5
kesle yrn <kesachyrn@gmail.com>
2024-02-14 12:56:13 +0200

    update todolist

--- server/index.js
+++ server/index.js
@@ -55,7 +55,19 @@
 
 
         //update a todo
+     
+        app.put("/todos/:id", async(req ,res ) =>{
+            try{
+            const { id } = req.params ;
+            const { description } = req.body
+            const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2 " , [description, id])
 
+            res.json("Todo was updated")
+        }catch(err){
+            console.error(err.massage);
+        }
+        }) 
+        
 
 
         //delete a todo
