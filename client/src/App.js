fb850bcf3122115305703aaa7d50f7a4984ff7f7
kesle yrn <kesachyrn@gmail.com>
2024-02-22 13:03:21 +0200

    updating to the latest

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './App.css';
 import InputTodo from './components/InputTodo';
 
-
-
 function App() {
   return (
-  <Fragment>
-    <InputTodo/>
-  </Fragment>
+    <>
+      <InputTodo />
+    </>
   );
 }
 
--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-user: "postgress",
-password:"kg",
-host:"localhost",
-port:5432,
-database:perntodolist,
-})
+  user: "postgress",
+  password: "kg",
+  host: "localhost",
+  port: 5432,
+  database: "perntodolist", 
+});
 
-module.exports = pool; 
+module.exports = pool;
